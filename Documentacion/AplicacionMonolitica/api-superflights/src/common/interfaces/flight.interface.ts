0589b433ec984d0db61a4a54ef15d97dcedf71dc
DESARROLLO2\DESARROLLO2 <victor_hugo_cruzazul@hotmail.com>
2022-03-14 12:12:04 -0700

    Obtener clima por vuelo

--- Documentacion/AplicacionMonolitica/api-superflights/src/common/interfaces/flight.interface.ts
+++ Documentacion/AplicacionMonolitica/api-superflights/src/common/interfaces/flight.interface.ts
@@ -1,11 +1,14 @@
 import { IPassenger } from "./passenger.interface";
+import { IWeather } from "./weather.location";
 
 export interface IFlight extends Document {
+    _id?: string;
     pilot: string;
     airplane: string;
     destinationCity: string;
     flightDate: Date;
     passengers: IPassenger[];
+    weather: IWeather
 }
 
 
--- Documentacion/AplicacionMonolitica/api-superflights/src/flight/flight.service.ts
+++ Documentacion/AplicacionMonolitica/api-superflights/src/flight/flight.service.ts
@@ -4,7 +4,10 @@ import { FLIGHT } from 'src/common/models/models';
 import { Model } from 'mongoose';
 import { IFlight } from 'src/common/interfaces/flight.interface';
 import { FlightDTO } from './dto/flight.dto';
-
+import axios from 'axios'
+import * as _moment from 'moment'
+import { ILocation } from 'src/common/interfaces/location.interface';
+import { IWeather } from 'src/common/interfaces/weather.location';
 @Injectable()
 export class FlightService {
     //@InjectModel(FLIGHT.name) decorador, l decimos a mongoose q tome el schema(modelo) flights para q lo inyecte, mongoose lo conoce porque lo importamos en flight.module.ts en la clase MongooseModule.
@@ -12,6 +15,22 @@ export class FlightService {
     //Inyectamos el schema(Flight.schema.ts) configurado para esta entidad y q los tipos para los valores sean de tipo IFlight
     constructor(@InjectModel(FLIGHT.name) private readonly model: Model<IFlight>) { }
 
+    async getLocation(destinationCity: string): Promise<ILocation> {
+        const { data } = await axios.get(`https://www.metaweather.com/api/location/search/?query=${destinationCity}`);
+        return data[0]; //recordar q l endpoint retorna una array con 1 item
+    }
+
+    async getWeather(woeid: number, flightDate: Date): Promise<IWeather[]> {
+        const dateFormat = _moment.utc(flightDate).format();
+        const year = dateFormat.substring(0, 4);
+        const month = dateFormat.substring(5, 7);
+        const day = dateFormat.substring(8, 10);
+        const { data } = await axios.get(`https://www.metaweather.com/api/location/${woeid}/${year}/${month}/${day}/`);
+        return data;
+    }
+    assign({ _id, pilot, airplane, destinationCity, flightDate, passengers }: IFlight, weather: IWeather[]): IFlight {
+        return Object.assign({ _id, pilot, airplane, destinationCity, flightDate, passengers, weather });
+    }
     async create(flightDTO: FlightDTO): Promise<IFlight> {
         //return await new this.model(flightDTO).save(); Ó
         const newFlight = new this.model(flightDTO); //creamos un documento(schema) de mongoose
@@ -19,11 +38,14 @@ export class FlightService {
     }
 
     async findAll(): Promise<IFlight[]> {
-        return this.model.find();
+        return this.model.find().populate('passengers');
     }
 
     async findOne(id: string): Promise<IFlight> {
-        return this.model.findById(id);
+        const flight = await this.model.findById(id).populate('passengers');
+        const location: ILocation = await this.getLocation(flight.destinationCity); //obtemer localdad
+        const weather: IWeather[] = await this.getWeather(location.woeid, flight.flightDate); //obtener clima
+        return this.assign(flight, weather)
     }
 
     async update(id: string, flight: FlightDTO): Promise<IFlight> {
